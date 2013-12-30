50fc393df0848cde4ceaeabc6907d89916b0801a
Anastasia Lyupa <a.lyupa@samsung.com>
2013-12-30 13:55:54 +0400

    [PROTO] limit packed str length by 256 bytes
    Change-Id: Ib134e2632acab7a4a0f4d1049755e1116d180b21
    Signed-off-by: Anastasia Lyupa <a.lyupa@samsung.com>

--- include/binproto.h
+++ include/binproto.h
@@ -94,9 +94,12 @@ static char __attribute__((used)) *pack_string(char *to, const char *str)
 		*to = '\0';
 		return to + 1;
 	} else {
-		size_t len = strlen(str) + 1;
-		strncpy(to, str, len);
-		return to + len;
+		enum { max_str_len = 255 };
+		size_t len = strnlen(str, max_str_len);
+		memcpy(to, str, len);
+		to += len;
+		*to = '\0';
+		return to + 1;
 	}
 }
 
@@ -133,7 +136,6 @@ static char __attribute__((used)) *pack_value_by_type(char *to, const char t, va
 	float f;
 	double w;
 	char *s;
-	int n;
 
 	*to++ = t;
 
@@ -172,9 +174,7 @@ static char __attribute__((used)) *pack_value_by_type(char *to, const char t, va
 		break;
 	case 's':
 		s = va_arg(*args, char *);
-		n = strlen(s) + 1;
-		strncpy(to, s, n);
-		to += n;
+		to = pack_string(to, s);
 		break;
 	case 'v':
 	case 'n':
