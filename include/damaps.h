7d95f2c497a8ffa86c08b7612b8ce06370f57f97
Vitaliy Cherepanov <v.cherepanov@samsung.com>
2014-07-15 09:46:58 +0400

    [FEATURE] internal external call instrumentation
    if the library is instrumented all ld probes for this lib will be packed
    if regular feture is enabled but always feature is not
    
    Change-Id: I07373c0dd69c15caf5c2305b6b416295af0eefe8
    Signed-off-by: Vitaliy Cherepanov <v.cherepanov@samsung.com>

---
+++ include/damaps.h
@@ -0,0 +1,33 @@
+/*
+ *  DA probe
+ *
+ * Copyright (c) 2000 - 2014 Samsung Electronics Co., Ltd. All rights reserved.
+ *
+ * Contact:
+ *
+ * Cherepanov Vitaliy <v.cherepanov@samsung.com>
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the
+ * Free Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT ANY
+ * WARRANTY; without even the implied warranty of MERCHANTABILITY or
+ * FITNESS FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public
+ * License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with this library; if not, write to the Free Software Foundation, Inc., 51
+ * Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA
+ *
+ * Contributors:
+ * - S-Core Co., Ltd
+ *
+ */
+
+
+extern int maps_init();
+extern void maps_make();
+extern int maps_is_instrument_section_by_addr(const void *addr);
+extern int set_map_inst_list(char **maps_set, uint32_t maps_count);
