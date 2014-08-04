faea95454b37ac7b544b522f27f32cd0b384d951
Vitaliy Cherepanov <v.cherepanov@samsung.com>
2014-08-04 13:23:17 +0400

    [FIX] da_io probes filering
    Change-Id: I9d0ee9a72b226ef049e547ab2634bb95ac270bb8
    Signed-off-by: Vitaliy Cherepanov <v.cherepanov@samsung.com>

--- probe_file/da_io_posix.c
+++ probe_file/da_io_posix.c
@@ -71,7 +71,8 @@ int open(const char* path, int oflag, ...)
 	char buffer[PATH_MAX];
 	int mode = 0;
 
-	BEFORE_ORIGINAL_FILE_NOFILTER(open, LIBC);
+	BEFORE_ORIGINAL_FILE(open, LIBC);
+	_filepath = (char*)path;
 
 	if(oflag & O_CREAT)
 	{
@@ -97,7 +98,8 @@ int openat(int fd, const char* path, int oflag, ...)
 	char buffer[PATH_MAX];
 	int mode = 0;
 
-	BEFORE_ORIGINAL_FILE_NOFILTER(openat, LIBC);
+	BEFORE_ORIGINAL_FILE(openat, LIBC);
+	_filepath = (char*)path;
 
 	if(oflag & O_CREAT)
 	{
@@ -122,7 +124,8 @@ int creat(const char* path, mode_t mode)
 	static int (*creatp)(const char* path, mode_t mode);
 	char buffer[PATH_MAX];
 
-	BEFORE_ORIGINAL_FILE_NOFILTER(creat, LIBC);
+	BEFORE_ORIGINAL_FILE(creat, LIBC);
+	_filepath = (char*)path;
 
 	ret = creatp(path, mode);
 
@@ -449,7 +452,7 @@ int dup(int fd)
 {
 	static int (*dupp)(int fd);
 
-	BEFORE_ORIGINAL_FILE_NOFILTER(dup, LIBC);
+	BEFORE_ORIGINAL_FILE(dup, LIBC);
 
 	ret = dupp(fd);
 
@@ -463,7 +466,7 @@ int dup2(int fd, int fd2)
 {
 	static int (*dup2p)(int fd, int fd2);
 
-	BEFORE_ORIGINAL_FILE_NOFILTER(dup2, LIBC);
+	BEFORE_ORIGINAL_FILE(dup2, LIBC);
 
 	ret = dup2p(fd, fd2);
 
