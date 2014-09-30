1f977429cd67ff07ac5692dade0195090c0b80ff
Vyacheslav Cherkashin <v.cherkashin@samsung.com>
2014-09-30 13:20:09 +0400

    [FIX] app terminate
    Change-Id: Ie9369556859bf4636fa8ffae884189ccec8403d2
    Signed-off-by: Vyacheslav Cherkashin <v.cherkashin@samsung.com>

--- helper/libdaprobe.c
+++ helper/libdaprobe.c
@@ -104,16 +104,11 @@ static void _configure(char* configstr)
 
 void application_exit()
 {
-	void (*elm_exit)(void);
-
 	PRINTMSG("App termination: EXIT(0)");
 	/* TODO think of another way for correct app termination */
 
-	elm_exit = dlsym(RTLD_DEFAULT, "elm_exit");
-	if (elm_exit)
-		elm_exit();
-
-	exit(0);
+	/* Kill yourself!! ]:> */
+	killpg(getpgrp(), SIGKILL);
 }
 
 // create socket to daemon and connect
