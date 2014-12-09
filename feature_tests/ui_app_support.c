8070a0291f3aa6f7e2bb950fc641584f20dbb188
Vyacheslav Cherkashin <v.cherkashin@samsung.com>
2014-12-09 21:23:12 +0400

    [FIX] building lifecycle probes
    add checking ui_app_main() support
    
    Change-Id: I6a27e96bd6d22743d1a3cb90fb89c089082f9599
    Signed-off-by: Vyacheslav Cherkashin <v.cherkashin@samsung.com>

---
+++ feature_tests/ui_app_support.c
@@ -0,0 +1,39 @@
+/*
+ *  DA probe
+ *
+ * Copyright (c) 2000 - 2014 Samsung Electronics Co., Ltd. All rights reserved.
+ *
+ * Contact:
+ *
+ * Vyacheslav Cherkashin <v.cherkashin@samsung.com>
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the
+ * Free Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT ANY
+ * WARRANTY; without even the implied warranty of MERCHANTABILITY or
+ * FITNESS FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public
+ * License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with this library; if not, write to the Free Software Foundation, Inc., 51
+ * Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA
+ *
+ * Contributors:
+ * - Samsung RnD Institute Russia
+ *
+ * Description:
+ *   Test app for checking ui_app_lifecycle_callback_s support.
+ *
+ */
+
+#include <app.h>
+
+int main(int argc, char *argv[])
+{
+	ui_app_lifecycle_callback_s __attribute__((unused)) callback;
+
+	return 0;
+}
