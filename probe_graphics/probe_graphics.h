0b29b7a3588f20861dd068a5a004025770800e5c
Alexander Aksenov <a.aksenov@samsung.com>
2016-04-13 11:42:02 +0300

    [FEATURE] GOT patcher: impelment
    Initial version, there are some more things to do:
    0. testing;
    1. getting original addresses from linker;
    2. cleanup after stop using;
    3. other TODOs.
    
    Change-Id: I8a0f2a590fabcdb439b3ec19c29f269379537b53
    Signed-off-by: Alexander Aksenov <a.aksenov@samsung.com>

---
+++ probe_graphics/probe_graphics.h
@@ -0,0 +1,7 @@
+#ifndef __PROBE_GRAPHICS_H__
+#define __PROBE_GRAPHICS_H__
+
+extern struct feature_desc_t *graphics_feature;
+extern struct feature_desc_t *graphics_feature_always;
+
+#endif /* __PROBE_GRAPHICS_H__ */
